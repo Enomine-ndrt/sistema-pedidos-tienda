8b3efa7b1e99e3edfd3d5634dcae7dcfa1c66a30
Enomine-ndrt <luis.w0587@gmail.com>
2026-07-29 01:04:04 -0600

    FIX:(kafka message)

--- pedido-service/src/main/java/com/corp/pedido_service/services/PedidoService.java
+++ pedido-service/src/main/java/com/corp/pedido_service/services/PedidoService.java
@@ -5,22 +5,46 @@ import com.corp.pedido_service.entities.Pedido;
 import com.corp.pedido_service.feingClients.InventarioCLient;
 import com.corp.pedido_service.repository.PedidoRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.ThreadLocalRandom;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class PedidoService {
 
     private final InventarioCLient inventarioCLient;
     private final PedidoRepository pedidoRepository;
-
+    private KafkaTemplate<String, String> kafkaTemplate;
 
     public Boolean crearPedido(Long productoId, Integer cantidad){
         ProductoDTO productoDTO = inventarioCLient.obtener(productoId);
+        int partition = ThreadLocalRandom.current().nextInt(2);
 
         if(productoDTO.getStock() < cantidad){
             return false;
         }
+
+        String mensaje = "";
+
+        if(productoDTO.getStock() < cantidad){
+            mensaje = "Pedido no creado: stock no suficiente ";
+        }else{
+            mensaje = "Pedido creado";
+        }
+
+        kafkaTemplate.send("str-topic",partition,null, mensaje).whenComplete((result, ex) -> {
+
+            if(ex != null){
+                log.error("Error , al enviar el mensaje: {}", ex.getMessage());
+            }
+            log.info("Mensaje enviado con exito: {}",result.getProducerRecord().value());
+            log.info("Particion {}, Offset {}",result.getRecordMetadata().partition(),
+                    result.getRecordMetadata().offset());
+        });
 
         Pedido pedido = new Pedido();
 
