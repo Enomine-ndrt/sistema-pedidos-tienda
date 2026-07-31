b087c81c7346f258ccce857a3ffbe47e6c764e0a
Enomine-ndrt <luis.w0587@gmail.com>
2026-07-31 17:08:36 -0600

    ADD:(config):websocket connection

--- inventario-service/src/main/java/com/corp/inventario_service/listeners/StrConsumerListener.java
+++ inventario-service/src/main/java/com/corp/inventario_service/listeners/StrConsumerListener.java
@@ -2,10 +2,12 @@ package com.corp.inventario_service.listeners;
 
 import com.corp.inventario_service.entities.Producto;
 import com.corp.inventario_service.events.PedidoCreadoEvent;
+import com.corp.inventario_service.events.StockActualizadoEvent;
 import com.corp.inventario_service.repository.ProductoRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
 
 
@@ -15,6 +17,7 @@ import org.springframework.stereotype.Component;
 public class StrConsumerListener {
 
     private final ProductoRepository repository;
+    private final SimpMessagingTemplate messagingTemplate;
 
     @KafkaListener(groupId = "inventario-group",
                     topics = "pedido-events",
@@ -33,6 +36,13 @@ public class StrConsumerListener {
         );
 
         repository.save(producto);
+        messagingTemplate.convertAndSend(
+                "/topic/stock",
+                new StockActualizadoEvent(
+                        producto.getId(),
+                        producto.getStock()
+                )
+        );
 
         log.info("Nuevo stock: {}", producto.getStock());
     }
