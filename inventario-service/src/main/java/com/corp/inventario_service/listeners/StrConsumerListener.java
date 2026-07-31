4ec392797d2c93aedf44b88d29c62a04922914db
Enomine-ndrt <luis.w0587@gmail.com>
2026-07-31 03:01:46 -0600

    ADD:(config):update stock

--- inventario-service/src/main/java/com/corp/inventario_service/listeners/StrConsumerListener.java
+++ inventario-service/src/main/java/com/corp/inventario_service/listeners/StrConsumerListener.java
@@ -1,6 +1,8 @@
 package com.corp.inventario_service.listeners;
 
+import com.corp.inventario_service.entities.Producto;
 import com.corp.inventario_service.events.PedidoCreadoEvent;
+import com.corp.inventario_service.repository.ProductoRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -12,6 +14,8 @@ import org.springframework.stereotype.Component;
 @Component
 public class StrConsumerListener {
 
+    private final ProductoRepository repository;
+
     @KafkaListener(groupId = "inventario-group",
                     topics = "pedido-events",
                     containerFactory = "kafkaListenerContainerFactory")
@@ -20,6 +24,17 @@ public class StrConsumerListener {
         log.info("ID Pedido: {}", event.getPedidoId());
         log.info("Producto: {}", event.getProductoId());
         log.info("Cantidad: {}", event.getCantidad());
+        Producto producto = repository
+                .findById(event.getProductoId())
+                .orElseThrow();
+
+        producto.setStock(
+                producto.getStock() - event.getCantidad()
+        );
+
+        repository.save(producto);
+
+        log.info("Nuevo stock: {}", producto.getStock());
     }
 
 }
