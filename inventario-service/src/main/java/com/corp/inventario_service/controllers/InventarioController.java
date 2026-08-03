5389899e307db151b6e74ee039af4f27aa2a5fcb
Enomine-ndrt <luis.w0587@gmail.com>
2026-08-03 02:43:47 -0600

    ADD:(config):add more stock

--- inventario-service/src/main/java/com/corp/inventario_service/controllers/InventarioController.java
+++ inventario-service/src/main/java/com/corp/inventario_service/controllers/InventarioController.java
@@ -1,9 +1,12 @@
 package com.corp.inventario_service.controllers;
 
+import com.corp.inventario_service.dto.AgregarStockRequest;
 import com.corp.inventario_service.entities.Producto;
+import com.corp.inventario_service.events.StockActualizadoEvent;
 import com.corp.inventario_service.repository.ProductoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,6 +17,7 @@ import java.util.List;
 public class InventarioController {
 
     private final ProductoRepository repository;
+    private final SimpMessagingTemplate messagingTemplate;
 
     @GetMapping("/{id}")
     public Producto obtener(@PathVariable Long id){
@@ -33,6 +37,43 @@ public class InventarioController {
     public ResponseEntity<Producto> save(@RequestBody Producto producto){
         Producto nuevoProducto =  repository.save(producto);
         return ResponseEntity.ok(nuevoProducto);
+    }
+
+    @PutMapping("/{id}/agregar")
+    public ResponseEntity<Producto> agregarStock(@PathVariable Long id, @RequestBody AgregarStockRequest request){
+
+        if(request.getCantidad() <= 0){
+            return ResponseEntity.badRequest().build();
+        }
+
+        Producto producto = repository.findById(id).orElseThrow(() ->
+                        new RuntimeException("Producto no encontrado")
+        );
+
+        producto.setStock(producto.getStock() + request.getCantidad());
+        Producto productoActualizado =  repository.save(producto);
+        messagingTemplate.convertAndSend(
+                "/topic/stock",
+                new StockActualizadoEvent(
+                        productoActualizado.getId(),
+                        productoActualizado.getStock()
+                )
+        );
+
+        return ResponseEntity.ok(productoActualizado);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> eliminarProducto(@PathVariable Long id){
+        Producto producto = repository.findById(id).orElseThrow(() ->
+                        new RuntimeException("Producto no encontrado")
+        );
+        repository.delete(producto);
+        messagingTemplate.convertAndSend(
+                "/topic/producto-eliminado",
+                producto.getId()
+        );
+        return ResponseEntity.noContent().build();
     }
 
 }
---
+++ inventario-service/src/main/java/com/corp/inventario_service/dto/AgregarStockRequest.java
@@ -0,0 +1,12 @@
+package com.corp.inventario_service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AgregarStockRequest {
+    private Integer cantidad;
+}
